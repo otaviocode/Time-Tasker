1847757d78c217e26ffa990e0b00b9d87a66f115
developer-otavio <developer-otavio@developer-otavio-H81M-S2PH>
2024-11-19 21:54:44 -0300

    teste 2

--- TimeTasker/src/main/java/com/example/demo/TimeTaskerApplication.java
+++ TimeTasker/src/main/java/com/example/demo/TimeTaskerApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+//ESSA LINHA É O TESTE DO COMMIT KKKKKKK SE VAI SINCRONIZAR!!!
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
