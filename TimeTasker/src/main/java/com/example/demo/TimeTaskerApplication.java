0989a2bd5b843b1a47b9ec116cfa0d35ae647454
developer-otavio <developer-otavio@developer-otavio-H81M-S2PH>
2024-12-01 23:10:45 -0300

    Commit Projeto Time Tasker

--- TimeTasker/src/main/java/com/example/demo/TimeTaskerApplication.java
+++ TimeTasker/src/main/java/com/example/demo/TimeTaskerApplication.java
@@ -1,8 +1,5 @@
 package com.example.demo;
 
-//ESSA LINHA É O TESTE DO COMMIT KKKKKKK SE VAI SINCRONIZAR!!! 
-//LAURA LANÇOU A BRABA DA PIZZA DIA 19-11-2024
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
